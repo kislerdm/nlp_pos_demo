8ab4895f37d083fdd89f378b834e29df3ef4ca6b
Dmitry Kisler <admin@dkisler.de>
2020-04-21 18:02:30 +0200

    fix env var name

--- app/.env.sh
+++ app/.env.sh
@@ -4,4 +4,4 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null && pwd )"
 
 export BUCKET_DATA=${BASE_DIR}/data
 export BUCKET_MODEL=${BASE_DIR}/model
-export MODEL_VER=v1
+export MODEL_VERSION=v1
