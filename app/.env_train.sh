2c2c44c5a47b1f47520aece31400a6f1d7c966b2
Dmitry Kisler <admin@dkisler.de>
2020-04-21 20:13:35 +0200

    final services compose files

--- app/.env_train.sh
+++ app/.env_train.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null && pwd )"
+BASE_DIR="$( cd "$(dirname "$0")"/.. >/dev/null 2>&1 ; pwd -P )"
 
 export BUCKET_DATA=${BASE_DIR}/data
 export BUCKET_MODEL=${BASE_DIR}/model
