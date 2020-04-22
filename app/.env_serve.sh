7be0d384080b44989a24c8798f733a41e6f9b200
Dmitry Kisler <admin@dkisler.de>
2020-04-22 04:10:04 +0200

    cleanup

--- app/.env_serve.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-BASE_DIR="$( cd "$(dirname "$0")"/.. >/dev/null 2>&1 ; pwd -P )"
-
-export BUCKET_DATA_INPUT=${BASE_DIR}/data/prediction/input
-export BUCKET_DATA_OUTPUT=${BASE_DIR}/data/prediction/output
-export BUCKET_MODEL=${BASE_DIR}/model
-export MODEL_VERSION=v1
--- app/.env_train.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-BASE_DIR="$( cd "$(dirname "$0")"/.. >/dev/null 2>&1 ; pwd -P )"
-
-export BUCKET_DATA=${BASE_DIR}/data/UD_English-GUM
-export BUCKET_MODEL=${BASE_DIR}/model
-export MODEL_VERSION=v1
