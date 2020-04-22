152b6329469c9ac65a270f55d32404d43f8ed38a
Dmitry Kisler <admin@dkisler.de>
2020-04-22 02:48:14 +0200

    fix env file

--- app/.env_train.sh
+++ app/.env_train.sh
@@ -2,6 +2,6 @@
 
 BASE_DIR="$( cd "$(dirname "$0")"/.. >/dev/null 2>&1 ; pwd -P )"
 
-export BUCKET_DATA=${BASE_DIR}/data
+export BUCKET_DATA=${BASE_DIR}/data/UD_English-GUM
 export BUCKET_MODEL=${BASE_DIR}/model
 export MODEL_VERSION=v1
